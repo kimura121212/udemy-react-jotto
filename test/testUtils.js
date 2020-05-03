956527eb793f7de607c40437c8a55487cbfefa7e
kimura121212 <ykimuranoi@gmail.com>
2020-05-03 17:12:55 +0900

    simple redux:success piece of state

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,11 @@
 import checkPropTypes from 'check-prop-types';
+import {createStore} from 'redux';
+
+import rootReducer from '../src/reducers';
+
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+}
 
 /**
  * Create a testing store with imported reducers, middleware, and initial state.
