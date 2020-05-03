5d3182258152099ca544b4169f052b46377942aa
kimura121212 <ykimuranoi@gmail.com>
2020-05-03 18:29:27 +0900

    testing redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,35 @@
+import {getLetterMatchCount} from '../helpers';
+
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
+    GUESS_WORD: 'GUESS_WORD',
+
 };
 
 export function correctGuess(){
     return {type: actionTypes.CORRECT_GUESS};
-}
+};
+
+/**
+ * Returns Redux Thunk function that dispatches GUESS_WORD action
+ *     and (conditionally) CORRECT_GUESS action
+ * @function guessWord
+ * @param {string} guessedWord - Guessed word.
+ * @returns {function} - Redux Thunk function.
+*/
+export const guessWord = (guessedWord) => {
+    return function(dispatch, getState) {
+      const secretWord = getState().secretWord;
+      const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+  
+      dispatch({
+        type: actionTypes.GUESS_WORD,
+        payload: { guessedWord, letterMatchCount }
+      });
+  
+      if (guessedWord === secretWord) {
+        dispatch({ type: actionTypes.CORRECT_GUESS });
+      }
+  
+    };
+  };
